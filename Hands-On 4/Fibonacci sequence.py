b161352ece03af98ea683749a2c4c429af056496
Rahul Varadaraju <135001490+Rahulv024@users.noreply.github.com>
2024-09-17 12:22:41 -0500

    Fibonacci sequence.py

--- Hands-On 4/Fibonacci sequence.py
+++ Hands-On 4/Fibonacci sequence.py
@@ -1,1 +1,10 @@
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+# Call fib(5) and print the result
+print(fib(5)) #output fib(5)=5
 
