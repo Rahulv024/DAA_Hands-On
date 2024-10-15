c7347ec75a86dcd7cc67c8eb6366eb69ae8c86f8
Rahul Varadaraju <rahulvaradaraju@gmail.com>
2024-10-15 10:28:12 -0500

    Implementing_ith_order.py

--- Hands-On8/Implementing_ith_order.py
+++ Hands-On8/Implementing_ith_order.py
@@ -1,1 +1,44 @@
+import random
 
+#partition for quicksort
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+    arr[i], arr[high] = arr[high], arr[i]
+    return i
+
+# Quicksort function to find ith order
+def quickselect(arr, low, high, i):
+    if low == high:
+        return arr[low]
+
+    # Partition the array
+    pivot_index = partition(arr, low, high)
+
+    # pivot is in its final sorted position
+    if i == pivot_index:
+        return arr[i]
+    elif i < pivot_index:
+        return quickselect(arr, low, pivot_index - 1, i)
+    else:
+        return quickselect(arr, pivot_index + 1, high, i)
+
+# Function to find ith smallest element
+def ith_order_statistic(arr, i):
+    if i < 0 or i >= len(arr):
+        raise IndexError("Index out of bounds")
+    return quickselect(arr, 0, len(arr) - 1, i)
+
+# Example
+arr = [24,10,7,18,23,99,66]
+i = 3  # Find the 4th smallest element as index starts from 0
+result = ith_order_statistic(arr, i)
+print(f"The {i+1}th smallest element is: {result}")
+
+
+#OUTPUT: 
+#The 4th smallest element is: 23
